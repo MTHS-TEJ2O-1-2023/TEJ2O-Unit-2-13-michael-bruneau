8c7026db9cb05879725f60da26f44f141aeefebf
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-10-31 16:39:10 -0400

    setup

--- main.ts
+++ main.ts
@@ -7,19 +7,28 @@
 
 // variables
 let loopCounter: number = 4
+let neopixelStrip: neopixel.Strip = null
 
 // setup
 basic.showIcon(IconNames.Happy)
 basic.pause(500)
 basic.clearScreen()
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(4, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
 
 input.onButtonPressed(Button.A, function () {
   basic.clearScreen()
   loopCounter = 4
 
-  while (loopCounter > 0) {
+  while (loopCounter > -1) {
     basic.pause(500)
     basic.showNumber(loopCounter)
     loopCounter = loopCounter - 1
   }
+  basic.clearScreen()
+  basic.showIcon(IconNames.Happy)
 })
